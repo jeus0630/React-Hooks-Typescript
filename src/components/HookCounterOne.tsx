ead05c0d453d7d18aa2546db6021e64bfd0457e5
jewoo <jeus0630@designfever.com>
2022-02-15 15:55:08 +0900

    docs : HookCounterOne.tsx
    useEffect only with count

--- src/components/HookCounterOne.tsx
+++ src/components/HookCounterOne.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, useEffect, useState} from 'react';
+import React, {ChangeEvent, FunctionComponent, useEffect, useState} from 'react';
 
 interface OwnProps {
 }
@@ -8,23 +8,29 @@ type Props = OwnProps;
 const HookCounterOne: FunctionComponent<Props> = (props) => {
 
     const [count, setCount] = useState(0);
-
+    const [name, setName] = useState('');
     useEffect(()=>{
-
+        console.log('updating title');
         document.title = `you clicked ${count} tiems`;
 
         return () => {
 
         }
 
-    })
+    },[count]);
 
     const clickHandler = () => {
         setCount(prev => prev+1);
     }
 
+    const changeHandler = (e: ChangeEvent) => {
+        const {value} = e.target as HTMLInputElement;
+        setName(value);
+    }
+
     return (
         <div>
+            <input type="text" onChange={changeHandler}/>
             <button onClick={clickHandler}>Click {count} times</button>
         </div>
     );
